8d03cd5039410532483512c2a38bd6bd81641826
HisayaMiyao <orange.380138@toki.waseda.jp>
2024-07-26 22:53:19 +0900

    introduced client component

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KIXJZ9oRFlyy2BEFBnIWe8dGvS0uvGgDgM6Wj/HBLMk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4GTah+mSOgg3NP4kMdPKskr0XZJYvr3VWqrwu176dI0=\"\n}"
